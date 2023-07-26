d252233a5ed155b1e1616a6b40e2560a7d700f9b
prabhattopi <topi9864@gmail.com>
2023-07-26 09:19:30 +0000

    the file is save

---
+++ pages/api/chat.ts
@@ -0,0 +1,116 @@
+import { Message } from "@/types";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+import pineconeStore from "@/utils/pineconeStore";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function translate(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { messages, userName } = req.body;
+
+  const translatedText = await askOpenAI({ messages, userName });
+
+  const TRIAL_URL = "https://api.elevenlabs.io";
+  const API_PATH = `/v1/text-to-speech/${process.env.ELEVENLABS_VOICE_ID}`;
+  const API_KEY = process.env.ELEVENLABS_KEY as string;
+
+  const OPTIONS = {
+    method: "POST",
+    body: JSON.stringify({
+      text: translatedText,
+      model_id: "eleven_monolingual_v1",
+    }),
+    headers: {
+      "xi-api-key": API_KEY,
+      "Content-Type": "application/json",
+      accept: "audio/mpeg",
+    },
+  };
+
+  const response = await fetch(TRIAL_URL + API_PATH, OPTIONS);
+
+  const audioData = await response.arrayBuffer();
+  const audioDataBase64 = Buffer.from(audioData).toString("base64");
+
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ audioDataBase64, translatedText }));
+}
+
+async function askOpenAI({
+  messages,
+  userName,
+}: {
+  messages: Message[];
+  userName: string;
+}) {
+  const pinecone = await pineconeStore();
+
+  console.log("messages req: ", messages);
+
+  // updated the message content to include context snippets
+  if (messages?.length > 0) {
+    const lastMsgContent = messages[messages.length - 1].content;
+
+    const data = await pinecone.similaritySearch(lastMsgContent, 3);
+
+    console.log("pinecone data.length: ", data.length);
+
+    const updatedMsgContent = `
+    user question/statement: ${lastMsgContent}
+    context snippets:
+    ---
+    1) ${data?.[0]?.pageContent}
+    ---
+    2) ${data?.[1]?.pageContent}
+    ---
+    3) ${data?.[2]?.pageContent}
+    `;
+
+    messages[messages.length - 1].content = updatedMsgContent;
+  }
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `
+        Imagine you are Naval Ravikant and you want to give advice to the user you're interacting with that may ask you questions or advice. The user's name is ${userName}.
+        I will provide you context snippets from "The Almanack of Naval Ravikant" from a vecor database to help you answer the user's questions.
+        Introduce youself to ${userName}. Don't mention context snippets when replying to user and only mention yourself by your first name.
+        `,
+        },
+        ...(messages || [
+          {
+            role: "user",
+            content: "Hi There!",
+          },
+        ]),
+      ],
+    });
+    console.log(response)
+    return response?.data?.choices?.[0]?.message?.content;
+} catch (error) {
+    if (error.response && error.response.status === 429) {
+      // Rate limit reached, retry after a delay
+      console.log("Rate limit exceeded. Retrying after a delay...");
+ 
+      return "There was an error in processing the AI response.";
+    } else {
+      console.log("Error in response:", error.message);
+      return "There was an error in processing the AI response.";
+    }
+  }
+}
+
+// async function delay(ms:number) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
